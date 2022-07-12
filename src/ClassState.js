d5a0029cfe8ca27c4134278bcaaed5bd4c1f5e1e
Misael-GC <misael.gomez1623@gmail.com>
2022-07-12 00:08:24 -0500

    Métodos del ciclo de vida en React.Component

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,36 +1,55 @@
 import React from "react";
+import { Loading } from "./Loading";
+class ClassState extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: false,
+      loading: false,//actualizar paso 1
+    };
+  }
 
-//vamos a llamar a la funcion super y enviar props, parara agregarle props a this
-class  ClassState extends React.Component{
-    constructor(props){
-        //tenemos que llamar a la función super cuando querramos modificar this en nuestra clase y que funcione
-        super(props);
-        this.state ={
-            error: false,
-        };
-    }
-    render(){
-        return(
-            <div>
-                <h2>Eliminar {this.props.name}</h2>
-                <p>Porfavor, write the segure code for know that wants delete</p>
+  //linea 12
+//   UNSAFE_componentWillMount(){
+//     console.log('componentWillMount')
+//   }
 
-                {this.state.error &&(
-                <p>Error: el código es incorrecto</p>
-            ) }
+//   componentDidMount(){
+//     console.log('componentDidMount')
+//   }
 
-                <input placeholder="código de seguridad"/>
-                <button
-                //vamos a decir cual es el estado que vamos a actualizar, en este caso negar el estado anterior
-                //te dejo 2 soluciones
-                // onClick={() => this.setState({error: !this.state.error})}
-                onClick={() => 
-                this.setState(prevState => ({error: !prevState.error}))
-                }
-                >Affirm</button>
-            </div>
-        );
-    }
+componentDidUpdate(){
+    console.log('actualización');
+
+    if(!!this.state.loading){ //-------------------
+        setTimeout(()=>{
+            console.log("Haciendo la validación")
+
+            this.setState({ loading:false }); //sintaxis paso 3
+
+            console.log('terminando la validación');
+        }, 3000);
+    } 
+} //despues de una actualización de un cambio ene el estado vamos a llamar a this.setState
+// es necesario e importante encapsularlo en una condicional porque de no ser asi causamos un bucle infinito al dar click al boton
+  render() {
+    return (
+      <div>
+        <h2>Eliminar {this.props.name}</h2>
+        <p>Porfavor, write the segure code for know that wants delete</p>
+
+        {this.state.error && <p>Error: el código es incorrecto</p>}
+        {this.state.loading && <Loading/>}
+
+        <input placeholder="código de seguridad" />
+        <button
+          onClick={() => this.setState({ loading:true })} //actualización paso 2
+        >
+          Affirm
+        </button>
+      </div>
+    );
+  }
 }
 
 export { ClassState };
